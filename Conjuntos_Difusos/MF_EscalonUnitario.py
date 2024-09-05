e2bb7a90e0a69e61a6d8a709ee0c1c39506b2054
Charlie Parra <106718268+CharlieParra@users.noreply.github.com>
2024-09-05 10:47:19 -0600

    Update MF_EscalonUnitario.py

--- Conjuntos_Difusos/MF_EscalonUnitario.py
+++ Conjuntos_Difusos/MF_EscalonUnitario.py
@@ -11,18 +11,14 @@ def MF_escalonUnitario(X, a):
     return 1 if X >= a else 0
 
 # Función para graficar la MF:
-def plot_MF_escalonUnitario(a, min_universo, max_universo, puntos, color):
+def plot_MF_escalonUnitario(a, min_universo, max_universo, puntos, color, negado):
     X_values = np.linspace(a - 0.000000001, a + 0.000000001, puntos)
     MF_values = [MF_escalonUnitario(X, a) for X in X_values]
+    
+    if negado == 1:
+        MF_values = [1 - val for val in MF_values]
+        label = f'Escalón Negado (a={a})'
+    else:
+        label = f'Escalón (a={a})'
 
-    # plt.figure(figsize=(10, 6))
-    plt.plot(X_values, MF_values, label=f'Step Membership Function (a={a})', color=color)
-
-    # plt.xlabel('X')
-    # plt.ylabel('Membership Value')
-    # plt.title('Step Membership Function - Flanco de Subida')
-    # plt.legend()
-    # plt.grid(True)
-    # plt.ylim(-0.1, 1.1)
-    # plt.xlim(min_universo, max_universo)
-    # plt.show()
+    plt.plot(X_values, MF_values, label=label, color=color)
