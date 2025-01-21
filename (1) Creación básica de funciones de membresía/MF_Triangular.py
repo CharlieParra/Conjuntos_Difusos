b25b81e1ce0adef5c35f9c05762493f14a84de08
Charlie Parra <106718268+CharlieParra@users.noreply.github.com>
2025-01-20 21:06:48 -0600

    Add files via upload

---
+++ (1) Creación básica de funciones de membresía/MF_Triangular.py
@@ -0,0 +1,62 @@
+# Función para evaluar una MF triangular:
+# X = Punto de evaluación.
+# a = Vértice izquierdo de la función triangular.
+# b = Vértice central de la función triangular.
+# c = Vértice derecho de la función triangular.
+# min_universo = Valor mínimo del conjunto universo.
+# max_universo = Valor máximo del conjunto universo.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Función para evaluar una MF triangular:
+def MF_triangular(X, a, b, c):
+    if X <= a:
+        return 0
+    elif a < X <= b:
+        return (1 / (b - a)) * (X - a)
+    elif b < X < c:
+        return -(1 / (c - b)) * (X - c)
+    else:
+        return 0
+
+# Función para solicitar los valores de a, b, c y el rango del conjunto universo:
+def tnMF_main():
+    while True:
+        a = float(input("Ingrese el valor de a: "))
+        b = float(input("Ingrese el valor de b: "))
+        c = float(input("Ingrese el valor de c: "))
+        if a < b < c:
+            break
+        else:
+            print("Error: El valor de a debe ser menor que b y el valor de b debe ser menor que c. Por favor, ingrese los valores nuevamente.")
+
+    while True:
+        min_universo = float(input("Ingrese el valor mínimo del conjunto universo: "))
+        max_universo = float(input("Ingrese el valor máximo del conjunto universo: "))
+        if min_universo < max_universo:
+            break
+        else:
+            print("Error: El valor mínimo del conjunto universo debe ser menor al valor máximo. Por favor, ingrese los valores nuevamente.")
+
+    plot_MF_triangular(a, b, c, min_universo, max_universo)
+
+# Función para graficar la MF:
+def plot_MF_triangular(a, b, c, min_universo, max_universo):
+    X_values = np.linspace(min_universo, max_universo, 500)
+    X_values = np.concatenate(([a, b, c], X_values))
+    X_values = np.sort(X_values)
+
+    MF_values = [MF_triangular(X, a, b, c) for X in X_values]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(X_values, MF_values, label=f'Triangular Membership Function (a={a}, b={b}, c={c})', color='blue')
+
+    plt.xlabel('X')
+    plt.ylabel('Membership Value')
+    plt.title('Triangular Membership Function')
+    plt.legend()
+    plt.grid(True)
+    plt.ylim(-0.1, 1.1)
+    plt.xlim(min_universo, max_universo)
+    plt.show()
